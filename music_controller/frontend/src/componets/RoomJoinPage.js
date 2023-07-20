7e1e007294e8efdcbb614892cef82b3d0d665079
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-20 12:39:13 -0300

    por problemas de compatibilidade tive que voltar a versão do react-router-dom e assim tive que mudar todos meu codigos

--- music_controller/frontend/src/componets/RoomJoinPage.js
+++ music_controller/frontend/src/componets/RoomJoinPage.js
@@ -1,6 +1,87 @@
 
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: "",
+      error: "",
+    };
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Join a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <TextField
+            error={this.state.error}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this.handleTextFieldChange}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.roomButtonPressed}
+          >
+            Enter Room
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  handleTextFieldChange(e) {
+    this.setState({
+      roomCode: e.target.value,
+    });
+  }
+
+  roomButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: this.state.roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          this.props.history.push(`/room/${this.state.roomCode}`);
+        } else {
+          this.setState({ error: "Room not found." });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+
+/*import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -13,7 +94,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 const RoomJoinPage = ()=>{
-    const navigate = useNavigate();
     //const [guestCanPause, setGuestCanPause] = useState(true);
     const [roomCode, setRoomCode] = useState('');
     const [error, setError] = useState('');
@@ -28,7 +108,7 @@ const RoomJoinPage = ()=>{
       };
       fetch('api/join-room', requestOptions).then((response) => {
         if (response.ok){
-          navigate("/room/" + roomCode)
+          this.props.history.push(`/room/${roomCode}`);
         } else{
           setError("Invalid Code")
         }
@@ -61,6 +141,8 @@ const RoomJoinPage = ()=>{
               </TextField>
             </FormControl>
           </Grid>
+
+          
           <Grid item xs={6} align="end">
             <Button variant="contained" color="primary" onClick={joinIn}  align="center">
               Join
