056f4637b4100555d99452efa2b2b1b75ca6351b
Lucas Alvws <sociallucasalves@gmail.com>
2023-07-20 13:25:12 -0300

    Aula 10 - ajustando a room page pra funcionar corretamente e criando o botao de leave room

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RoomView,CreateRoomView, GetRoom, JoinRoomView, UserInRoom
+from .views import RoomView,CreateRoomView, GetRoom, JoinRoomView, UserInRoom, LeaveRoomView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoomView.as_view()),
     path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoomView.as_view()),
 
 ]
--- music_controller/frontend/src/componets/Room.js
+++ music_controller/frontend/src/componets/Room.js
@@ -1,7 +1,94 @@
+import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
+export default class Room extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+    };
+    this.roomCode = this.props.match.params.roomCode;
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    this.getRoomDetails();
+  }
 
+  getRoomDetails() {
+    fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+      });
+  }
 
-import React, { useEffect, useState } from "react";
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Grid container spacing={1}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h4" component="h4">
+              Code: {this.roomCode}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h5" component="h5">
+              Votes: {this.state.votesToSkip}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h5" component="h5">
+              Guest Can Pause: {this.state.guestCanPause.toString()}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h5" component="h5">
+              Host: {this.state.isHost.toString()}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.leaveButtonPressed}
+            >
+              Leave Room
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+/*<div>
+        <h3>{this.roomCode}</h3>
+        <p>Votes: {this.state.votesToSkip}</p>
+        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+        <p>Host: {this.state.isHost.toString()}</p>
+      </div>*/
+
+/*import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
@@ -34,7 +121,7 @@ const Room = () => {
         setGuestCanPause(data.guest_can_pause);
         setVotesToSkip(data.votes_to_skip);
         setIsHost(data.is_host);})
-    })*/
+    })
 
   return (
     <div>
@@ -46,8 +133,7 @@ const Room = () => {
   );
 };
 
-export default Room;
-
+export default Room;*/
 
 /*import React, {Component} from "react";
 import { useParams } from 'react-router-dom';
